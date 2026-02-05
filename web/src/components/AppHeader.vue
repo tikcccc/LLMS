048468618b073b876198744575e488feaa83a0a7
tikcccc <chiutikhong11551@gmail.com>
2026-02-05 16:51:22 +0800

    1

--- web/src/components/AppHeader.vue
+++ web/src/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="app-header">
     <div class="brand">
-      <div class="title">Digital Land Manag Demo</div>
+      <div class="title">Digital Land Management Platform</div>
     </div>
 
     <div class="actions">
